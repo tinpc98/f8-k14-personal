66783a2cd91170b75780333a9676f3df3cfb88b3
quangtin <tinpcf8@fullstack.edu.vn>
2025-07-28 01:49:20 +0700

    BKStar

---
+++ next/2025-07-12/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import {useState} from "react";
+
+export default function Home() {
+  const [count, setCount] = useState<number>(0)
+  return (
+    <>
+      <h2>count: {count}</h2>
+    </>
+  );
+}
